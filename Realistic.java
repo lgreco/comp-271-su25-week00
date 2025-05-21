1361dcb30b5189e2acb6762f9d2070efe6f5fd8b
Leo Irakliotis <lgreco@gmail.com>
2025-05-21 16:02:47 +0000

    remove and return

--- Realistic.java
+++ Realistic.java
@@ -8,16 +8,38 @@ public class Realistic {
      * @return int with the smallest value in array arr
      */
     public static int getSmallest(int[] arr) {
-        // Assume that the first element is the smallest
-        int index_smallest = 0;
-        // Loop through the rest of the array to find something smaller
+        int smallest_index = 0; // Assume smallest is first element
         for (int i = 1; i < arr.length; i++) {
-            // check if current element at position [i] is smaller
-            if (arr[i] < arr[index_smallest]) {
-                index_smallest = i;
-            }
+            if (arr[i] < arr[smallest_index]) {
+                smallest_index = i; // found new smallest, update index
+            } 
+        } // when loop is done, smallest_index points to smallest element
+        int result = arr[smallest_index];
+        // Reduce the input array to omit smallest element about to return
+        int[] temporary = new int[arr.length -1];
+
+        /* Using two for loops 
+        for (int i = 0; i < smallest_index; i++) {
+            temporary[i] = arr[i];
         }
-        return arr[index_smallest];
+        for (int i = smallest_index+1; i < arr.length; i++) {
+            temporary[i-1] = arr[i];
+        } */
+
+        /* Using a while loop */
+        int temp_index = 0;
+        int arr_index = 0;
+        while (arr_index < arr.length) {
+            if ( arr_index != smallest_index) {
+                temporary[temp_index] = arr[arr_index];
+                temp_index++;
+            }
+            arr_index++;
+        }
+
+        arr = temporary; // input array shorted by 1 (removing smallest elem)
+        return result; // smallest element
+        
     } // method getSmallest
 
     public static void main(String[] args) {
