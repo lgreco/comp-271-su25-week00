afa1e764225922a7910d680269f2aa656720b5b1
Leo Irakliotis <lgreco@gmail.com>
2025-05-21 18:19:16 -0500

    realistic

--- Realistic.java
+++ Realistic.java
@@ -1,50 +1,72 @@
-import java.util.Arrays;
+import java.util.Arrays; // from printing array
 
+/**
+ * A class to demonstrate minimum heap operations using arrays
+ */
 public class Realistic {
 
+    /** Set up our test array. */
+    static int[] arr = { 10, -5, 11, 2 };
+
     /**
-     * Scan the entire array to find its smallest value
-     * @param arr int[] array to explore
+     * Scan the entire array to find and remove its smallest value.
+     * The method uses array arr[] created above.
+     * 
      * @return int with the smallest value in array arr
      */
-    public static int getSmallest(int[] arr) {
-        int smallest_index = 0; // Assume smallest is first element
+    public static int getSmallest() {
+        // Assume smallest is first element
+        int smallest_index = 0;
+        // Scan the remaining elements, replacing the position of the smallest element
+        // with the position of any element found to be smaller.
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[smallest_index]) {
-                smallest_index = i; // found new smallest, update index
-            } 
-        } // when loop is done, smallest_index points to smallest element
+                // found new smallest, update index
+                smallest_index = i;
+            }
+        }
+        // When loop is done, smallest_index points to smallest element. Save it in a
+        // variable so that we can return its value when we are done.
         int result = arr[smallest_index];
-        // Reduce the input array to omit smallest element about to return
-        int[] temporary = new int[arr.length -1];
+        // Prepare to shrink the processed array by one element, effectively removing
+        // its smallest element. A temporary array will hold the remaining elements.
+        int[] temporary = new int[arr.length - 1];
 
-        /* Using two for loops 
+        /*
+         * Use two loops to copy the elements of arr[] to temporary[], except for the
+         * smallest element. The first loop copies all the elements before the smallest
+         * element and the second loop the elements after it.
+         */
         for (int i = 0; i < smallest_index; i++) {
             temporary[i] = arr[i];
         }
-        for (int i = smallest_index+1; i < arr.length; i++) {
-            temporary[i-1] = arr[i];
-        } */
-
-        /* Using a while loop */
-        int temp_index = 0;
-        int arr_index = 0;
-        while (arr_index < arr.length) {
-            if ( arr_index != smallest_index) {
-                temporary[temp_index] = arr[arr_index];
-                temp_index++;
-            }
-            arr_index++;
+        for (int i = smallest_index + 1; i < arr.length; i++) {
+            temporary[i - 1] = arr[i];
         }
+        /*
+         * Same result with a while loop
+         * 
+         * int temp_index = 0;
+         * int arr_index = 0;
+         * while (arr_index < arr.length) {
+         * if (arr_index != smallest_index) {
+         * temporary[temp_index] = arr[arr_index];
+         * temp_index++;
+         * }
+         * arr_index++;
+         * }
+         */
 
-        arr = temporary; // input array shorted by 1 (removing smallest elem)
+        arr = temporary; // array shorted by 1 (removing smallest elem)
         return result; // smallest element
-        
     } // method getSmallest
 
+    /** Driver/simple test code */
     public static void main(String[] args) {
-        int[] testArray = {10, -5, 11, 2};
-        System.out.println(getSmallest(testArray));
-        System.out.println(Arrays.toString(testArray));
-    }
-}
+        System.out.printf("\n\nArray before removal of smallest element: %s",
+                Arrays.toString(arr));
+        System.out.printf("\nSmallest element found: %d", getSmallest());
+        System.out.printf("\nArray after removal of smallest element: %s\n\n",
+                Arrays.toString(arr));
+    } // method main
+} // class Realistic
